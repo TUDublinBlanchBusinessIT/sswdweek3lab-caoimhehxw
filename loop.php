49a69c420293f0989a41acd8aa11da9156e2f1f8
B00153330 Caoimhe Walsh <B00153330@mytudublin.ie>
2024-10-02 11:02:03 +0100

    completed

---
+++ loop.php
@@ -0,0 +1,14 @@
+<?php
+    echo"<table border = '1'>";
+    for ($i=1;$i<=15;$i++) {
+        echo"<TR>";
+        for ($j=1;$j<=20;$j++){
+            echo "<td>Row $i, Column $j</TD>";
+
+
+        }
+       echo"</TR>";
+    
+    }
+    echo "</table>";
+    ?>
